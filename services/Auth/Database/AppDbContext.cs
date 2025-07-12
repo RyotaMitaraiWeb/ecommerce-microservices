e28a9a04026dc79b6d327731c061ef494b5a459b
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-07-12 15:46:47 +0300

    configure DB Context

---
+++ services/Auth/Database/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
--- services/Auth/Database/Entities/User.cs
+++ services/Auth/Database/Entities/User.cs
@@ -1,9 +1,16 @@
-﻿namespace Database.Entities
+﻿using Common.User;
+using System.ComponentModel.DataAnnotations;
+
+namespace Database.Entities
 {
     public class User
     {
+        [Key]
         public Guid Id { get; set; }
+
+        [MaxLength(UserValidationRules.Email.MaxLength)]
         public string Email { get; set; } = string.Empty;
+
         public string PasswordHash { get; set; } = string.Empty;
     }
 }
