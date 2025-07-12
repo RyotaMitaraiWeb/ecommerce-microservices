715078361d19b70c393136ce205f058b902bdb3a
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-07-12 16:06:38 +0300

    add extension method for services

---
+++ services/Auth/Auth.Web/Extensions/ServicesExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Auth.Web.Extensions
+{
+    public static class ServicesExtensions
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            return services
+                .AddLogging()
+                .AddCors();
+        }
+    }
+}
--- services/Auth/Auth.Web/Program.cs
+++ services/Auth/Auth.Web/Program.cs
@@ -1,3 +1,4 @@
+using Auth.Web.Extensions;
 
 namespace Auth.Web
 {
@@ -13,6 +14,7 @@ namespace Auth.Web
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddServices(builder);
 
             var app = builder.Build();
 
