383bfd231c1fa432c397ef76d232928e15ba7c06
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-07-19 23:32:12 +0300

    (auth service) try to specify port

--- services/Auth/Auth.Web/Program.cs
+++ services/Auth/Auth.Web/Program.cs
@@ -11,6 +11,8 @@ namespace Auth.Web
             builder.Services.AddServices(builder);
             builder.Services.AddBearerAuthentication(builder);
             builder.Services.AddDatabase(builder);
+            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+            builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
             var app = builder.Build();
 
