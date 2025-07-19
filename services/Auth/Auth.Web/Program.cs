2c5e40d00ce6ff8573379b327cb2c233d84c361e
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-07-19 23:36:07 +0300

    make swagger available in production

--- services/Auth/Auth.Web/Program.cs
+++ services/Auth/Auth.Web/Program.cs
@@ -16,12 +16,8 @@ namespace Auth.Web
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
