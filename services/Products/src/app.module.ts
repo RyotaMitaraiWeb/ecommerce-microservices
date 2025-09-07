68b056862cedd3b1dd7f1038d9f4e438bf1e65f1
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-09-08 01:21:47 +0300

    initial typeorm config

--- services/Products/src/app.module.ts
+++ services/Products/src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
