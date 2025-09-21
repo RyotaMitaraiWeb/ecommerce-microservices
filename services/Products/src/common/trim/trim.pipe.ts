e36904a8ea530a60c654b9f7cee59e1f75649e6f
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-09-21 19:59:53 +0300

    create a global trim pipe

---
+++ services/Products/src/common/trim/trim.pipe.ts
@@ -0,0 +1,27 @@
+import { PipeTransform, Injectable } from '@nestjs/common';
+
+type TrimmableValue = string | object | TrimmableValue[];
+type TrimmedValue = TrimmableValue;
+
+@Injectable()
+export class TrimPipe implements PipeTransform {
+  transform(value: unknown): TrimmedValue {
+    if (typeof value === 'string') {
+      return value.trim();
+    }
+
+    if (Array.isArray(value)) {
+      return value.map((element) => this.transform(element));
+    }
+
+    if (typeof value === 'object' && value !== null) {
+      for (const key in value) {
+        if (Object.prototype.hasOwnProperty.call(value, key)) {
+          value[key] = this.transform(value[key]);
+        }
+      }
+    }
+
+    return value as TrimmedValue;
+  }
+}
--- services/Products/src/main.ts
+++ services/Products/src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { TrimPipe } from './common/trim/trim.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalPipes(new TrimPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Products')
