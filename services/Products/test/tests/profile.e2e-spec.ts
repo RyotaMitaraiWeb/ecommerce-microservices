e00034b60d497a52771dbd4af2116e6a4a24598d
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-09-28 23:16:02 +0300

    write tests for the ID endpoint

--- services/Products/test/tests/profile.e2e-spec.ts
+++ services/Products/test/tests/profile.e2e-spec.ts
@@ -1,9 +1,14 @@
-import { HttpStatus, INestApplication } from '@nestjs/common';
+import {
+  HttpStatus,
+  INestApplication,
+  NotFoundException,
+} from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
 import { restartTables } from '../util/restartTables';
 import { profiles } from '../seeders/seedProfiles';
 import { ProfileDto } from 'src/profiles/dto/profile.dto';
+import { getProfileErrorMessages } from 'src/profiles/constants/erroMessages';
 
 describe('ProfilesController (e2e)', () => {
   let app: INestApplication<App>;
@@ -29,6 +34,43 @@ describe('ProfilesController (e2e)', () => {
         profile.createdAt.getTime(),
       );
       expect(body.id).toBe(1);
+    });
+
+    it('Returns 404 if no profile with such ID can be found', async () => {
+      const response = await request(app.getHttpServer()).get(
+        '/profiles/1555555',
+      );
+
+      expect(response.status).toBe(HttpStatus.NOT_FOUND);
+
+      const body = response.body as NotFoundException;
+      expect(body.message).toBe(getProfileErrorMessages.doesNotExist);
+    });
+
+    it('Returns 404 if the profile is deleted', async () => {
+      const profile = profiles.find((profile) => !profile.confirmed)!;
+
+      const response = await request(app.getHttpServer()).get(
+        `/profiles/${profile.id}`,
+      );
+
+      expect(response.status).toBe(HttpStatus.NOT_FOUND);
+
+      const body = response.body as NotFoundException;
+      expect(body.message).toBe(getProfileErrorMessages.doesNotExist);
+    });
+
+    it('Returns 404 if the profile is not confirmed', async () => {
+      const profile = profiles.find((profile) => !!profile.deletedAt)!;
+
+      const response = await request(app.getHttpServer()).get(
+        `/profiles/${profile.id}`,
+      );
+
+      expect(response.status).toBe(HttpStatus.NOT_FOUND);
+
+      const body = response.body as NotFoundException;
+      expect(body.message).toBe(getProfileErrorMessages.doesNotExist);
     });
   });
 });
