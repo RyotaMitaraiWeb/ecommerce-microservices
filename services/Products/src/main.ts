7361b96edcd7e138623c6d3e2dc0aeeef6952355
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-10-06 19:38:49 +0300

    start microservices in products service

--- services/Products/src/main.ts
+++ services/Products/src/main.ts
@@ -42,6 +42,7 @@ async function bootstrap() {
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger/api', app, documentFactory);
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
