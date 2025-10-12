5080195a3f0bf97399f4ba2bf730d73958189ec1
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-10-12 10:49:26 +0300

    inject queue in env variables

--- services/Auth/ProductsApi/Services/ProductApiService.cs
+++ services/Auth/ProductsApi/Services/ProductApiService.cs
@@ -1,18 +1,20 @@
 ﻿using Channel.Services.Contracts;
+using Microsoft.Extensions.Configuration;
 using ProductsApi.Dto;
 using ProductsApi.Services.Contracts;
 
 namespace ProductsApi.Services
 {
-    public class ProductApiService(IChannelService channelService) : IProductApiService
+    public class ProductApiService(IConfiguration config, IChannelService channelService) : IProductApiService
     {
+        private readonly string queue = config["RABBITMQ_INIT_PROFILE_QUEUE"] ?? throw new NullReferenceException(queue);
         public async Task<InitializeProfileResultDto> InitializeProfile(InitializeProfilePayloadDto payload)
         {
             NestRpcResponse<InitializeProfileResultDto> result = await channelService
                 .PublishRpcMessage<InitializeProfilePayloadDto, NestRpcResponse<InitializeProfileResultDto>>(
                     payload: payload,
                     pattern: "init_profile",
-                    queue: "profile_init");
+                    queue: queue);
 
             return result.Response!;
         }
