7b5fe006a1ac0e35364c17620c0c14550e278010
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-10-19 00:42:18 +0300

    remove unused usings

--- services/Auth/ProductsApi/Retry/RetryProfileInit.cs
+++ services/Auth/ProductsApi/Retry/RetryProfileInit.cs
@@ -1,11 +1,5 @@
 ﻿using Common.Retry;
 using Polly.Retry;
-using ProductsApi.Constants;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProductsApi.Retry
 {
